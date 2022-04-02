2f0fac82f906005879418fa539d321efa2851b11
kiwi-fruitiwi <zane.tian@gmail.com>
2022-04-02 11:22:46 -0700

    add one track playback.

--- sketch.js
+++ sketch.js
@@ -23,6 +23,15 @@
  *  mido.py's printing of a MIDI message; each .mid file is full of these
  *   Message('note_on', channel=0, note=66, velocity=80, time=13),
  *
+ *  The tone.js midi→js translator ends up with output like this:
+ *  {
+ *    "name": "G4",
+ *    "midi": 67,
+ *    "time": 1.37931,
+ *    "velocity": 0.6299212598425197,
+ *    "duration": 0.13045973750000006
+ *  },
+ *
  *  colxi's midi-parser-js's JSON message. the 'data' element is [note, vel]
  *   {
  *     "deltaTime": 13,
@@ -39,60 +48,83 @@
 let font
 let instructions
 
+
+let midiJSON /* json file containing translated midi msgs from tone.js */
+let tracks = []
+let track0_notes = [] /* track 0's notes */
+let notes = [] /* list of note objects created from track 0's JSON notes */
+let start /* starting time of sound playback. used as offset */
+let started = false
+
 let midiNotes = []
 let noteIndex = 0
 let midiVal, freq
 let osc
 let env
 
+let DEBUG_TEXT = 'hello! press T to start playback'
+let notePos = 0 /* current note in the notes list */
+
 
 function preload() {
     font = loadFont('data/consola.ttf')
+    midiJSON = loadJSON('midi-json/tone.js/toccata.json')
 }
 
 
 function setup() {
     let cnv = createCanvas(600, 300)
     cnv.parent('#canvas')
+
     colorMode(HSB, 360, 100, 100, 100)
-    textFont(font, 14)
+    textFont(font, 12)
+
 
     /* initialize instruction div */
     instructions = select('#ins')
     instructions.html(`<pre>
-        [1,2,3,4,5] → no function
+        [1,2,3,4,5] → no function 🐳
         s → cycle through MIDI sounds
+        t → iterate through midi notes and play them
         z → freeze sketch</pre>`)
 
-    osc = new p5.TriOsc();
-    env = new p5.Envelope();
 
-    for (let i=40; i<80; i++) {
-        midiNotes.push(i)
-    }
+    setupOscillator()
+    notes = createNotes(midiJSON['tracks'][0]['notes'])
+    console.log(notes)
+
+    /* track data → console.log(midiJSON['tracks']) */
 }
 
 
 function draw() {
     background(234, 34, 24)
     displayDebugCorner()
-}
 
+    /* if we've started:
+        check notes[0]
+        if notes[i].time * 1000 > offset + millis():
+            use oscillator to play that note
+            index++
+     */
+    if (started) {
+        if (millis() > notes[notePos].timestamp * 1000 + start) {
+            midiVal = notes[notePos].noteID;
+            freq = midiToFreq(midiVal);
+            console.log(freq)
+            osc.freq(freq);
+            env.ramp(osc, 0, 1.0, 0);
 
-/** 🧹 shows debugging info using text() 🧹 */
-function displayDebugCorner() {
-    const LEFT_MARGIN = 10
-    const DEBUG_Y_OFFSET = height - 10 /* floor of debug corner */
-    const LINE_SPACING = 2
-    const LINE_HEIGHT = textAscent() + textDescent() + LINE_SPACING
-    fill(0, 0, 100, 100) /* white */
-    strokeWeight(0)
+            DEBUG_TEXT = `ID: ${notes[notePos].name}, freq: ${freq.toFixed(2)} Hz`
+            notePos++
 
-    if (midiVal) {
-        text(`MIDI: ${midiVal}`,
-            LEFT_MARGIN, DEBUG_Y_OFFSET - LINE_HEIGHT)
-        text(`freq: ${freq.toFixed(1)} Hz`,
-            LEFT_MARGIN, DEBUG_Y_OFFSET)
+            /* automatically reset if we reach the end of the song */
+            if (notePos >= notes.length) {
+                started = false
+                DEBUG_TEXT = `press T again to start the music!`
+                notePos = 0 /* reset our position */
+            }
+        }
     }
 }
 
@@ -108,6 +140,59 @@ function keyPressed() {
     if (key === 's') {
         startSound()
     }
+
+    if (key === 't') {
+        start = millis()
+        started = true
+        osc.start()
+    }
+}
+
+
+/* iterates through arr and plays notes! */
+function playNotes(arr) {
+    osc.start()
+
+    for (let note in arr) {
+        freq = midiToFreq(midiVal)
+        osc.freq(freq)
+        env.ramp(osc, 0, 1.0, 0)
+    }
+}
+
+
+/* convert notes in tone.js midi translation into objects */
+function createNotes(notesList) {
+    let result = []
+    for (const note of notesList) {
+        /* name, noteID, timestamp, velocity, duration */
+        result.push(new Note(
+            note['name'],
+            note['midi'],
+            note['time'],
+            note['velocity'],
+            note['duration']
+        ))
+    }
+
+    return result
+}
+
+
+function logTrackKeys() {
+    for (let key in midiJSON['tracks'][0])
+        console.log(key)
+}
+
+
+function setupOscillator() {
+    /* set up oscillator to produce sounds */
+    osc = new p5.TriOsc();
+    env = new p5.Envelope();
+
+    for (let i=40; i<80; i++) {
+        midiNotes.push(i)
+    }
 }
 
 
@@ -120,4 +205,25 @@ function startSound() {
     osc.freq(freq);
     env.ramp(osc, 0, 1.0, 0);
     noteIndex++;
+}
+
+
+/** 🧹 shows debugging info using text() 🧹 */
+function displayDebugCorner() {
+    const LEFT_MARGIN = 10
+    const DEBUG_Y_OFFSET = height - 10 /* floor of debug corner */
+    const LINE_SPACING = 2
+    const LINE_HEIGHT = textAscent() + textDescent() + LINE_SPACING
+    fill(0, 0, 100, 100) /* white */
+    strokeWeight(0)
+
+    if (midiVal) {
+        text(`MIDI: ${midiVal}`,
+            LEFT_MARGIN, DEBUG_Y_OFFSET - 2*LINE_HEIGHT)
+        text(`freq: ${freq.toFixed(1)} Hz`,
+            LEFT_MARGIN, DEBUG_Y_OFFSET - LINE_HEIGHT)
+    }
+
+    text(`debug: ${DEBUG_TEXT}`,
+        LEFT_MARGIN, DEBUG_Y_OFFSET)
 }
